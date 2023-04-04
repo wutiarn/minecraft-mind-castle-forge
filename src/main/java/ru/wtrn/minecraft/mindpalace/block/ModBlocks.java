45169ce49e6d08bf41b8e6aea7479036caa3f820
Dmitry Romanov <me@wtrn.ru>
2023-04-05 00:03:58 +0300

    Setup dumb block meta

--- src/main/java/ru/wtrn/minecraft/mindpalace/block/ModBlocks.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/block/ModBlocks.java
@@ -18,7 +18,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, WtrnMindPalaceMod.MOD_ID);
 
-    public static final RegistryObject<Block> IMAGE_BLOCK = registerBlock("image",
+    public static final RegistryObject<Block> IMAGE_BLOCK = registerBlock("image_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.GLASS)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {

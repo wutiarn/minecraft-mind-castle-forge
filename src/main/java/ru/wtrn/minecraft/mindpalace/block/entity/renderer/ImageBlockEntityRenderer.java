4d835fff92a76a198278b9f109eeec7c384da4c4
Dmitry Romanov <me@wtrn.ru>
2023-04-05 00:20:34 +0300

    WIP: ImageBlockEntityRenderer

---
+++ src/main/java/ru/wtrn/minecraft/mindpalace/block/entity/renderer/ImageBlockEntityRenderer.java
@@ -0,0 +1,13 @@
+package ru.wtrn.minecraft.mindpalace.block.entity.renderer;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import ru.wtrn.minecraft.mindpalace.block.entity.ImageBlockEntity;
+
+public class ImageBlockEntityRenderer implements BlockEntityRenderer<ImageBlockEntity> {
+    @Override
+    public void render(ImageBlockEntity entity, float p_112308_, PoseStack poseStack, MultiBufferSource multiBufferSource, int pPackedLight, int pPackedOverlay) {
+
+    }
+}

fec926f00515ea98eed656a2bd429e36cada480e
Dmitry Romanov <me@wtrn.ru>
2023-04-05 01:11:28 +0300

    WIP: ImageBlockEntityRenderer

--- src/main/java/ru/wtrn/minecraft/mindpalace/block/entity/renderer/ImageBlockEntityRenderer.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/block/entity/renderer/ImageBlockEntityRenderer.java
@@ -9,6 +9,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 import org.slf4j.Logger;
 import ru.wtrn.minecraft.mindpalace.block.entity.ImageBlockEntity;
 
@@ -18,9 +21,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.ByteBuffer;
 
 public class ImageBlockEntityRenderer implements BlockEntityRenderer<ImageBlockEntity> {
     private static final Logger LOGGER = LogUtils.getLogger();
+    private int textureId = -1;
+    private BufferedImage bufferedImage;
 
     public ImageBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
         LOGGER.info("Initializing ImageBlockEntityRenderer");
@@ -49,9 +55,70 @@ public class ImageBlockEntityRenderer implements BlockEntityRenderer<ImageBlockE
         LOGGER.info("Image loaded");
     }
 
+    private int getTextureId() {
+        if (textureId == -1) {
+            textureId = uploadTexture(bufferedImage);
+        }
+        LOGGER.info("Image uploaded to texture");
+        return textureId;
+    }
+
+    private int uploadTexture(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[] pixels = new int[width * height];
+        image.getRGB(0, 0, width, height, pixels, 0, width);
+        boolean hasAlpha = false;
+        if (image.getColorModel().hasAlpha()) {
+            for (int pixel : pixels) {
+                if ((pixel >> 24 & 0xFF) < 0xFF) {
+                    hasAlpha = true;
+                    break;
+                }
+            }
+        }
+        int bytesPerPixel = hasAlpha ? 4 : 3;
+        ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * bytesPerPixel);
+        for (int pixel : pixels) {
+            buffer.put((byte) ((pixel >> 16) & 0xFF)); // Red component
+            buffer.put((byte) ((pixel >> 8) & 0xFF)); // Green component
+            buffer.put((byte) (pixel & 0xFF)); // Blue component
+            if (hasAlpha) {
+                buffer.put((byte) ((pixel >> 24) & 0xFF)); // Alpha component. Only for RGBA
+            }
+        }
+        buffer.flip();
+
+        int textureID = GlStateManager._genTexture(); //Generate texture ID
+        RenderSystem.bindTexture(textureID); //Bind texture ID
+
+        //Setup wrap mode
+        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+
+        //Setup texture scaling filtering
+        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+
+        if (!hasAlpha)
+            RenderSystem.pixelStore(GL11.GL_UNPACK_ALIGNMENT, 1);
+
+        // fixes random crash, when values are too high it causes a jvm crash, caused weird behavior when game is paused
+        GlStateManager._pixelStore(3314, 0);
+        GlStateManager._pixelStore(3316, 0);
+        GlStateManager._pixelStore(3315, 0);
+
+        //Send texel data to OpenGL
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, hasAlpha ? GL11.GL_RGBA8 : GL11.GL_RGB8, width, height, 0, hasAlpha ? GL11.GL_RGBA : GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
+
+        //Return the texture ID so we can bind it later again
+        return textureID;
+    }
+
 
     @Override
     public void render(ImageBlockEntity entity, float pPartialTick, PoseStack poseStack, MultiBufferSource multiBufferSource, int pPackedLight, int pPackedOverlay) {
+        int textureId = getTextureId();
 //        RenderSystem.enableDepthTest();
 //        RenderSystem.enableBlend();
 //        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);

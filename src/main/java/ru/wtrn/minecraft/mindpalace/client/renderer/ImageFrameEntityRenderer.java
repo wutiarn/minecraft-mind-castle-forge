ac21a0a6d34c4d820d4e11572a924bdeadf90929
Dmitry Romanov <me@wtrn.ru>
2023-04-07 20:46:27 +0300

    WIP: CachedTexture

--- src/main/java/ru/wtrn/minecraft/mindpalace/client/renderer/ImageFrameEntityRenderer.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/client/renderer/ImageFrameEntityRenderer.java
@@ -18,6 +18,8 @@ import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 import org.slf4j.Logger;
+import ru.wtrn.minecraft.mindpalace.client.texture.CachedTexture;
+import ru.wtrn.minecraft.mindpalace.client.texture.TextureCache;
 import ru.wtrn.minecraft.mindpalace.items.ImageFrame;
 import ru.wtrn.minecraft.mindpalace.util.math.base.Facing;
 import ru.wtrn.minecraft.mindpalace.util.math.box.AlignedBox;
@@ -34,99 +36,13 @@ import java.nio.ByteBuffer;
 
 public class ImageFrameEntityRenderer extends EntityRenderer<ImageFrame> {
     private static final Logger LOGGER = LogUtils.getLogger();
-    private int textureId = -1;
-    private BufferedImage bufferedImage;
+    private final CachedTexture cachedTexture = TextureCache.get("http://100.64.1.3:8094/storage/i/2.jpg");
 
     public ImageFrameEntityRenderer(EntityRendererProvider.Context pContext) {
         super(pContext);
-        LOGGER.info("Initializing ImageBlockEntityRenderer");
-        try {
-            loadTexture();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        cachedTexture.incrementUsageCounter();
     }
 
-    private void loadTexture() throws IOException {
-        String url = "https://i.imgur.com/M6UFOku.png";
-        HttpURLConnection httpURLConnection = (HttpURLConnection) (new URL(url)).openConnection(Minecraft.getInstance().getProxy());
-        try {
-            httpURLConnection.setDoInput(true);
-            httpURLConnection.setDoOutput(false);
-            httpURLConnection.connect();
-
-            InputStream inputStream = httpURLConnection.getInputStream();
-            bufferedImage = ImageIO.read(inputStream);
-        } finally {
-            httpURLConnection.disconnect();
-        }
-
-        LOGGER.info("Image loaded");
-    }
-
-    private int getTextureId() {
-        if (bufferedImage == null) {
-            return -1;
-        }
-        if (textureId == -1) {
-            LOGGER.info("Uploading image to texture");
-            textureId = uploadTexture(bufferedImage);
-            LOGGER.info("Image uploaded to texture");
-        }
-        return textureId;
-    }
-
-    private int uploadTexture(BufferedImage image) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        int[] pixels = new int[width * height];
-        image.getRGB(0, 0, width, height, pixels, 0, width);
-        boolean hasAlpha = false;
-        if (image.getColorModel().hasAlpha()) {
-            for (int pixel : pixels) {
-                if ((pixel >> 24 & 0xFF) < 0xFF) {
-                    hasAlpha = true;
-                    break;
-                }
-            }
-        }
-        int bytesPerPixel = hasAlpha ? 4 : 3;
-        ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * bytesPerPixel);
-        for (int pixel : pixels) {
-            buffer.put((byte) ((pixel >> 16) & 0xFF)); // Red component
-            buffer.put((byte) ((pixel >> 8) & 0xFF)); // Green component
-            buffer.put((byte) (pixel & 0xFF)); // Blue component
-            if (hasAlpha) {
-                buffer.put((byte) ((pixel >> 24) & 0xFF)); // Alpha component. Only for RGBA
-            }
-        }
-        buffer.flip();
-
-        int textureID = GlStateManager._genTexture(); //Generate texture ID
-        RenderSystem.bindTexture(textureID); //Bind texture ID
-
-        //Setup wrap mode
-        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
-        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
-
-        //Setup texture scaling filtering
-        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-        RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-
-        if (!hasAlpha)
-            RenderSystem.pixelStore(GL11.GL_UNPACK_ALIGNMENT, 1);
-
-        // fixes random crash, when values are too high it causes a jvm crash, caused weird behavior when game is paused
-        GlStateManager._pixelStore(3314, 0);
-        GlStateManager._pixelStore(3316, 0);
-        GlStateManager._pixelStore(3315, 0);
-
-        //Send texel data to OpenGL
-        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, hasAlpha ? GL11.GL_RGBA8 : GL11.GL_RGB8, width, height, 0, hasAlpha ? GL11.GL_RGBA : GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
-
-        //Return the texture ID so we can bind it later again
-        return textureID;
-    }
 
     @Override
     public boolean shouldRender(ImageFrame pLivingEntity, Frustum pCamera, double pCamX, double pCamY, double pCamZ) {
@@ -139,7 +55,7 @@ public class ImageFrameEntityRenderer extends EntityRenderer<ImageFrame> {
         RenderSystem.enableBlend();
         RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
 //        RenderSystem.setShaderColor(frame.brightness, frame.brightness, frame.brightness, frame.alpha);
-        int textureId = getTextureId();
+        int textureId = cachedTexture.getTextureId();
 
         RenderSystem.bindTexture(textureId);
         RenderSystem.setShaderTexture(0, textureId);
--- src/main/java/ru/wtrn/minecraft/mindpalace/client/texture/TextureCache.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/client/texture/TextureCache.java
@@ -1,11 +1,23 @@
 package ru.wtrn.minecraft.mindpalace.client.texture;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.HashMap;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TextureCache {
-    private static final HashMap<String, TextureCache> cached = new HashMap<>();
+    private static final ConcurrentHashMap<String, CachedTexture> cached = new ConcurrentHashMap<>();
 
+    public static CachedTexture get(String url) {
+        return cached.computeIfAbsent(url, CachedTexture::new);
+    }
+
+    @SubscribeEvent
+    public static void unload(LevelEvent.Unload event) {
+        if (event.getLevel().isClientSide()) {
+            for (CachedTexture cache : cached.values())
+                cache.cleanup();
+            cached.clear();
+        }
+    }
 }

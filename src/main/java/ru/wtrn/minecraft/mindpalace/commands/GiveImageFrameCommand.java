0d828b2a61dad6a488b8f58ffebbdb17a9f5e8b1
Dmitry Romanov <me@wtrn.ru>
2023-04-08 12:43:02 +0300

    WIP: Size modification command

--- src/main/java/ru/wtrn/minecraft/mindpalace/commands/GiveImageFrameCommand.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/commands/GiveImageFrameCommand.java
@@ -4,11 +4,12 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.LongArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.server.command.EnumArgument;
+import ru.wtrn.minecraft.mindpalace.items.ImageFrame;
 import ru.wtrn.minecraft.mindpalace.items.ImageFrameItem;
 import ru.wtrn.minecraft.mindpalace.items.ModItems;
 
@@ -18,18 +19,29 @@ public class GiveImageFrameCommand {
         dispatcher.register(
                 Commands.literal("i")
                         .then(
-                                Commands.argument("image_id", LongArgumentType.longArg()).executes(GiveImageFrameCommand::execute)
+                                Commands.argument("image_id", LongArgumentType.longArg()).executes(GiveImageFrameCommand::giveImage)
+                        )
+                        .then(
+                                Commands.argument("orientation", EnumArgument.enumArgument(ImageFrame.TargetSizeSide.class))
+                                        .then(Commands.argument("size", IntegerArgumentType.integer(0, 100)))
+                                        .executes(GiveImageFrameCommand::setImageSize)
                         )
         );
     }
 
-    public static int execute(CommandContext<CommandSourceStack> context) {
+    public static int giveImage(CommandContext<CommandSourceStack> context) {
         long image_id = context.getArgument("image_id", Long.class);
         ImageFrameItem item = ModItems.IMAGE_FRAME_ITEM.get();
         ItemStack stack = new ItemStack(item, 1);
         item.setImageId(stack, image_id);
         context.getSource().getPlayer().getInventory().add(stack);
         context.getSource().sendSuccess(Component.literal("Given " + item.getName(stack).getString()), true);
+        return 0;
+    }
+
+    public static int setImageSize(CommandContext<CommandSourceStack> context) {
+        ImageFrame.TargetSizeSide orientation = context.getArgument("orientation", ImageFrame.TargetSizeSide.class);
+        long size = context.getArgument("size", Integer.class);
         return 0;
     }
 }

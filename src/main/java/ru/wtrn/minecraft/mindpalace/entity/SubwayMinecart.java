b635529153764687619ab9d5379bf0a30e9ed3b6
Dmitry Romanov <me@wtrn.ru>
2023-06-17 19:48:22 +0300

    WIP: SubwayMinecart

--- src/main/java/ru/wtrn/minecraft/mindpalace/entity/SubwayMinecart.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/entity/SubwayMinecart.java
@@ -1,15 +1,26 @@
 package ru.wtrn.minecraft.mindpalace.entity;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 
-public class SubwayMinecart extends Minecart {
+public class SubwayMinecart extends AbstractMinecart {
     public SubwayMinecart(EntityType<?> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
+    @Override
+    protected Item getDropItem() {
+        return null;
+    }
+
+    @Override
+    public Type getMinecartType() {
+        return null;
+    }
+
     public SubwayMinecart(Level pLevel, double pX, double pY, double pZ) {
-        super(pLevel, pX, pY, pZ);
+        super(ModEntities.SUBWAY_MINECART_ENTITY.get(), pLevel, pX, pY, pZ);
     }
 }

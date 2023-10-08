299119ca5db1d1879e8c7f75e3582500eded919d
Dmitry Romanov <me@wtrn.ru>
2023-10-08 18:24:27 +0300

    WIP: RoutingRailBlockEntity

--- src/main/java/ru/wtrn/minecraft/mindpalace/entity/ModEntities.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/entity/ModEntities.java
@@ -3,6 +3,7 @@ package ru.wtrn.minecraft.mindpalace.entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,15 +12,25 @@ import ru.wtrn.minecraft.mindpalace.WtrnMindPalaceMod;
 import ru.wtrn.minecraft.mindpalace.items.ImageFrame;
 import ru.wtrn.minecraft.mindpalace.items.ImageFrameItem;
 
+import static ru.wtrn.minecraft.mindpalace.block.ModBlocks.ROUTING_RAIL_BLOCK;
+
 public class ModEntities {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
             DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, WtrnMindPalaceMod.MOD_ID);
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES =
+            DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, WtrnMindPalaceMod.MOD_ID);
 
     public static final RegistryObject<EntityType<ImageFrame>> IMAGE_FRAME_ENTITY = ENTITY_TYPES.register("image_frame_entity",
             () -> {
                 EntityType.Builder<ImageFrame> builder = EntityType.Builder.<ImageFrame>of(ImageFrame::new, MobCategory.MISC)
                         .sized(0.5F, 0.5F).clientTrackingRange(10).updateInterval(Integer.MAX_VALUE);
                 return builder.build("image_frame_entity");
+            });
+
+    public static final RegistryObject<BlockEntityType<RoutingRailBlockEntity>> ROUTING_RAIL_BLOCK_ENTITY = BLOCK_ENTITY_TYPES.register("routing_rail_block_entity",
+            () -> {
+                BlockEntityType.Builder<RoutingRailBlockEntity> builder = BlockEntityType.Builder.of(RoutingRailBlockEntity::new, ROUTING_RAIL_BLOCK.get());
+                return builder.build(null);
             });
 
 
---
+++ src/main/java/ru/wtrn/minecraft/mindpalace/entity/RoutingRailBlockEntity.java
@@ -0,0 +1,14 @@
+package ru.wtrn.minecraft.mindpalace.entity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+import static ru.wtrn.minecraft.mindpalace.entity.ModEntities.ROUTING_RAIL_BLOCK_ENTITY;
+
+public class RoutingRailBlockEntity extends BlockEntity {
+    public RoutingRailBlockEntity(BlockPos pPos, BlockState pBlockState) {
+        super(ROUTING_RAIL_BLOCK_ENTITY.get(), pPos, pBlockState);
+    }
+}

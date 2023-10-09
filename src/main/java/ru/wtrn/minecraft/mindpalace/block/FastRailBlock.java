a270d4f4d5ce7fcb93717ea1413edf2cc6eff483
Dmitry Romanov <me@wtrn.ru>
2023-10-09 10:03:41 +0300

    WIP: Disable physics for minecarts in FastRailBlock

--- src/main/java/ru/wtrn/minecraft/mindpalace/block/FastRailBlock.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/block/FastRailBlock.java
@@ -45,6 +45,7 @@ public class FastRailBlock extends RailBlock {
 
         double maxJumpPath = highSpeed - baseSpeed;
         if (maxJumpPath > 0) {
+            cart.noPhysics = true;
             performJump(pos, maxJumpPath, cart, level, directionVector);
         }
         cart.setDeltaMovement(directionVector.scale(baseSpeed));

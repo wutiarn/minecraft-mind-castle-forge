1d794954f4503e72f0c34b8566cea2aba5dfefe8
Dmitry Romanov <me@wtrn.ru>
2023-10-09 18:13:37 +0300

    WIP: RoutingService

--- src/main/java/ru/wtrn/minecraft/mindpalace/routing/RoutingService.java
+++ src/main/java/ru/wtrn/minecraft/mindpalace/routing/RoutingService.java
@@ -1,19 +1,23 @@
 package ru.wtrn.minecraft.mindpalace.routing;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import ru.wtrn.minecraft.mindpalace.block.ModBlocks;
 import ru.wtrn.minecraft.mindpalace.block.RoutingRailBlock;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RoutingService {
     public static RoutingService INSTANCE = new RoutingService();
+    private DefaultDirectedWeightedGraph<RoutingNode, Long> graph = null;
 
     public void rebuildGraph(BlockPos startBlockPos, Level level, Player player) {
         MinecraftServer server = level.getServer();
@@ -29,6 +33,17 @@ public class RoutingService {
         Collection<RoutingNode> discoveredNodes = new RoutesGraphBuilder(getRoutingRailBlock(), level).buildGraph(startBlockPos, null);
         String debugString = discoveredNodes.stream().map(RoutingNode::toString).collect(Collectors.joining("\n"));
         player.sendSystemMessage(Component.literal("Discovered nodes:\n" + debugString));
+
+        DefaultDirectedWeightedGraph<RoutingNode, Long> newGraph = new DefaultDirectedWeightedGraph<>(Long.class);
+        for (RoutingNode discoveredNode : discoveredNodes) {
+            newGraph.addVertex(discoveredNode);
+            for (Map.Entry<Direction, RoutingNode.Connection> connectionEntry : discoveredNode.connections.entrySet()) {
+                RoutingNode.Connection connection = connectionEntry.getValue();
+                newGraph.addVertex(connection.peer());
+                newGraph.addEdge(discoveredNode, connection.peer(), (long) connection.distance());
+            }
+        }
+        this.graph = newGraph;
     }
 
     public void onRoutingRailPlaced(BlockPos pos, Level level) {
